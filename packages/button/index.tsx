fc624da7e500231dffb0173315df47b0d6ceba55
375898312@qq.com <zcc256134@163.com>
2019-11-14 12:51:59 +0800

    feat: init

--- packages/button/index.tsx
+++ packages/button/index.tsx
@@ -1,22 +1,22 @@
-import { ButtonProps } from "./type";
-import { createComponent, h } from "vue3";
+import { ButtonProps } from './type'
+import { createComponent, h } from 'vue3'
 
 const buttonProps: any = {
   shape: String,
   loading: Boolean,
   disabled: Boolean,
   size: String,
-  type: { type: String, default: "default" },
-  htmlType: { type: String, default: "button" },
-  icon: { type: String, default: "" },
+  type: { type: String, default: 'default' },
+  htmlType: { type: String, default: 'button' },
+  icon: { type: String, default: '' },
   ghost: { type: Boolean, default: false }
-};
+}
 
 const Button = createComponent<ButtonProps, {}, {}>({
-  name: "Button",
+  name: 'Button',
   props: buttonProps,
   setup: (props, { slots, attrs }) => () => {
-    const { type, size, loading, icon, ghost } = props;
+    const { type, size, loading, icon, ghost } = props
     const cls = [
       attrs.class,
       `at-btn`,
@@ -27,22 +27,19 @@ const Button = createComponent<ButtonProps, {}, {}>({
         [`at-btn---icon-only`]: icon || loading,
         [`at-btn---${type}--hollow`]: ghost
       }
-    ];
-    const defSlot =
-      slots.default && h("span", { class: "at-btn__text" }, slots.default());
-    const iconSlot =
-      icon && !loading && h("i", { class: ["at-btn__icon icon", icon] });
-    const loadingSlot =
-      loading && h("i", { class: "at-btn__loading icon icon-loader" });
+    ]
+    const defSlot = slots.default && h('span', { class: 'at-btn__text' }, slots.default())
+    const iconSlot = icon && !loading && h('i', { class: ['at-btn__icon icon', icon] })
+    const loadingSlot = loading && h('i', { class: 'at-btn__loading icon icon-loader' })
     const data = {
       disabled: props.disabled,
       type: props.htmlType,
       style: attrs.style,
       class: cls
-    };
+    }
 
-    return h("button", data, [iconSlot, loadingSlot, defSlot]);
+    return h('button', data, [iconSlot, loadingSlot, defSlot])
   }
-});
+})
 
-export default Button;
+export default Button
